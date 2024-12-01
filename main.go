12f49ffe9910bfa8cafcf3afcd9bb88d5c6d6866
Lorenzo Catania <lcatania@Mac.fritz.box>
2024-12-01 12:56:10 +0100

    Added seperation line to output

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Day1 Result -", day1.Day1())
+	fmt.Println("Day1 Result -`", day1.Day1())
 	fmt.Println("Day1 Part 2 Result -", day1.Day1Part2())
+	fmt.Println("-----------------------------")
 }
